d724fff31108e0f7f3361f14d01f118d5d3c1347
Liao-Zike <105149061+Liao-Zike@users.noreply.github.com>
2022-10-14 21:40:56 +0800

    Update coollight.js

--- coollight.js
+++ coollight.js
@@ -2,16 +2,17 @@ import{neonCursor} from "./coollightchd.js"
   var win = document.getElementById('app');//抓取寬度
   var wid = win.offsetWidth;  //寬度
   var big=105;
-if(wid>=750)
+if(wid>=750){
   big=105;
-else if(wid<750&&wid>=360)
-  big=40;
-
+}
+else if(wid<750){
+  big=35;//40
+}
 neonCursor({
   el: document.getElementById('app'),
   shaderPoints: 20,//20
-  curvePoints: 100,//長度
-  curveLerp: 0.6,//速度
+  curvePoints: 200,//長度
+  curveLerp: 0.8,//速度
   radius1: 5,//粗
   radius2: 5,//周圍顏色
   velocityTreshold: 2,
